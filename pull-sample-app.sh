f4f748b1008a652c54cb7b45e77e1bc2fa8e826d
thepetk <thepetk@gmail.com>
2024-07-23 18:45:24 +0100

    update samples

--- pull-sample-app.sh
+++ pull-sample-app.sh
@@ -2,7 +2,7 @@ ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 CHATBOT_DIR=$ROOT_DIR/chatbot
 CODEGEN_DIR=$ROOT_DIR/codegen
-INSTRUCTLAB_DIR=$ROOT_DIR/chatbot
+INSTRUCTLAB_DIR=$ROOT_DIR/ilab
 
 REPO="https://github.com/containers/ai-lab-recipes"
 
@@ -16,6 +16,6 @@ REPONAME=$(basename $REPO)
 
 cp -r $TEMPDIR/$REPONAME/recipes/natural_language_processing/chatbot/app/ $CHATBOT_DIR/
 cp -r $TEMPDIR/$REPONAME/recipes/natural_language_processing/codegen/app/ $CODEGEN_DIR/  
-cp -r $TEMPDIR/$REPONAME/recipes/natural_language_processing/instructlab/app/ $INSTRUCTLAB_DIR/ 
+cp -r $TEMPDIR/$REPONAME/recipes/natural_language_processing/ilab/app/ $INSTRUCTLAB_DIR/ 
 
 rm -rf $TEMPDIR # clean up
